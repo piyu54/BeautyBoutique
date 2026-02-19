0afc15235427b6bef85d4ff37c11626d957969e6
unknown <priyakushwaha1407@gmail.com>
2026-02-19 23:33:06 +0530

    commit

--- src/com/GuestOperation/GuestOperation.java
+++ src/com/GuestOperation/GuestOperation.java
@@ -10,7 +10,7 @@ import com.UserOperation.UserRegistration;
 public class GuestOperation 
 {
 	public void getGuestOperation() throws SQLException {
-	System.out.println("**************Firstly you have to Register to buy a Product*****************");
+	System.out.println("************Firstly you have to Register to buy a Product**************");
 		
 	UserRegistration registration = new UserRegistration();
 	registration.getRegistration();
